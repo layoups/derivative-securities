0d1b698fd8cf783311f38dbb9d1b64dcae41ef0a
layoups <layounkarim@gmail.com>
2021-05-04 14:29:30 -0400

    heston:

--- portfolio_sim.m
+++ portfolio_sim.m
@@ -3,7 +3,6 @@ clear all
 
 
 %% simulation parameters
-
 returns_mat = table_to_list("Returns Data.csv", 0);
 prices_mat = table_to_list("Stock Data.csv", 0);
 
@@ -18,8 +17,9 @@ DeltaTime = 1/360;
 nobs = 360;
 nTrials = 20;
 
-%% multi gbm
+num = size(sigma);
 
+%% multi gbm
 gbm = gbm(mean_returns, sigma, ...
     'StartState', start, ...
     'correlation', corr_returns);
@@ -47,7 +47,6 @@ mert = merton(mean_returns, sigma, ...
     'correlation', corr_returns);
 
 %% jump sim
-
 merton_stocks = simulate(mert, nobs, ...
  'DeltaTime', DeltaTime, ...
  'nTrials', nTrials);
@@ -59,7 +58,6 @@ tsla_merton_sim = squeeze(merton_stocks(:, 4, :));
 cvx_merton_sim = squeeze(merton_stocks(:, 5, :));
 dal_merton_sim = squeeze(merton_stocks(:, 6, :));
 
-
 %% CEV parameters
 alphas = table_to_list("stock_gammas.csv", 0)';
 
@@ -69,7 +67,6 @@ CEV = cev(mean_returns, alphas, sigma, ...
     'correlation', corr_returns);
 
 %% CEV sim
-
 CEV_stocks = simulate(CEV, nobs, ...
  'DeltaTime', DeltaTime, ...
  'nTrials', nTrials);
@@ -80,4 +77,26 @@ pfe_CEV_sim = squeeze(CEV_stocks(:, 3, :));
 tsla_CEV_sim = squeeze(CEV_stocks(:, 4, :));
 cvx_CEV_sim = squeeze(CEV_stocks(:, 5, :));
 dal_CEV_sim = squeeze(CEV_stocks(:, 6, :));
+
+%% Heston Parameters
+level = ones(num(1), 1);
+speed = ones(num(1), 1);
+volvol = 0.2;
+
+%% Heston Model
+Heston = heston(mean_returns, speed, level, volvol, ...
+    'correlation', corr_returns, ...    
+    'StartState', start);
+
+%% Heston sim
+heston_stocks = simulate(Heston, nobs, ...
+ 'DeltaTime', DeltaTime, ...
+ 'nTrials', nTrials);
+
+aapl_Heston_sim = squeeze(Heston_stocks(:, 1, :));
+jpm_Heston_sim = squeeze(Heston_stocks(:, 2, :));
+pfe_Heston_sim = squeeze(Heston_stocks(:, 3, :));
+tsla_Heston_sim = squeeze(Heston_stocks(:, 4, :));
+cvx_Heston_sim = squeeze(Heston_stocks(:, 5, :));
+dal_Heston_sim = squeeze(Heston_stocks(:, 6, :));
 
